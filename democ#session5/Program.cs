f926276d89c5494fccfbde94975065bd5b82185c
ADMIN <ADMIN@DESKTOP-HOB8HCV>
2024-12-22 22:43:00 +0200

    Add project files.

---
+++ democ#session5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace democ_session5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

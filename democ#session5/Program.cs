801a711ce523550c171af21c5c4ff3c9cbc7eabf
Greg Low <greg@gmail.com>
2024-12-24 05:53:11 +0200

    Nullable types

--- democ#session5/Program.cs
+++ democ#session5/Program.cs
@@ -1,43 +1,48 @@
-﻿namespace democ_session5
+﻿using System.ComponentModel.Design;
+
+namespace democ_session5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
-            #region Boxingandunboxing
-            //boxing>casting from value type to reference type 
-            //un Boxing>casting from reference type to value type
-            // Object O1;
-            // O1 =>can refer to instance of "object" or any instance of any type
-            // O1 = 1;
-            // O1 = "Ahmed";
-            //O1 = 3 ; //case from value type [int] to reference type [object] => Boxing
-            //O1 = 1.5 ; //case from value type [double] to reference type [object] => Boxing
-            //O1 = 'A' ; //case from value type [char] to reference type [object] => Boxing
-            // O1 = new datatime ();// case from value type [date time] to reference type [object] => Boxing
-            //01 = true  ; // case from value type [bool] to reference type [object] => Boxing
-            int X = 4;
-            object O1 = X;
-            //IMPLICIT CASTING 
-            //BOXING 
-            //SAFE CASTING
-            //PARENT = CHILD => CHILD IS PARENT
-            //ANIMAL = DOG => DOG IS ANIMAL
+            //Nullable types
+            //value type               
+            //reference type
+            //int x = 6;
+            //x = null;   not valid 
+            //nullable value type => value type allow null as valid value 
+            //nullable <int> : allow int value or null as valid value 
+            //int?Age = 20;
+            //Age = null;
+            //nullable <double> : allow double value or null as valid value 
+            //double ?salary = 400.5;
+            //salary = null;
 
-            Object O2 = 3;
-            int Y = (int)O1;
-
-            //EXPLICIT CASTING 
-            //UN BOXING 
-            //UN SAFE CASTING [MAY THROW EXEPTION]
-            //CHILD = PARENT => PARENT IS CHILD
-            //DOG = ANIMAL => ANIMAL IS DOG
-            #endregion
+            //int x = 5;
+            //int ? y = x;
+            //Console.WriteLine(y);  // 5
+            //y: nullable <int>
+            //y: allow int value or null
+            //---implicit casting 
+            //---- safe casting 
+            //int? x = 5;
+            //x = null;
+            //y: int 
+            // y : allow int value only 
+            // int y =  x;  < int y = (int)x;
+            //explicit casting  // may throw exception
 
 
+            //Defensive code 
+            // if (x != null)  || x is not valid{
+            // Y = (int)x;
+            //else 
+            // y = 0;
+            // Console.WriteLine(y);
+       // }
 
-             
         }
     }
 }

716a6c142534f8e86d1137a3bc0df6080874cbb0
Greg Low <greg@gmail.com>
2024-12-24 08:04:27 +0200

    null propagation operator

--- democ#session5/Program.cs
+++ democ#session5/Program.cs
@@ -6,12 +6,27 @@ namespace democ_session5
     {
         static void Main(string[] args)
         {
-            //nullable reference type 
-            //string messageo1 = null; // required
-            //string? messageo2 = null;  // nullable
-            //Console.WriteLine(messageo1);
-            //Console.WriteLine(messageo2);
-            //Console.WriteLine(messageo1.Length); // null reference exception
+            //null propagation operator ?
+            //double x = default; 
+            //Console.WriteLine(x); //0
+            //char c = default;
+            //Console.WriteLine(c); //space
+            //bool c = default;
+            //Console.WriteLine(c);
+
+            // the default value with reference type is nuul 
+            //int[] Arr = default;
+            //for (int i = 0; i < args.Length; i++)
+            //    Console.WriteLine(Arr[i]); // null reference exception
+            //int[] Arr = { 1, 2, 3, 4, 5 };
+            ////Best practice 
+            //if (Arr is not null)
+            //{
+            //    for (int i = 0; i < args.Length; i++)
+            //        Console.WriteLine(Arr[i]);
+            //}
+            //int Len = arr.Length;
+            //Console.WriteLine(Len);
         }
     }
 }

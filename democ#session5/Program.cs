f0a9643a67f70e07d72e9c8f9cf475031eddef83
Greg Low <greg@gmail.com>
2024-12-24 08:42:58 +0200

    protectivecode

--- democ#session5/Program.cs
+++ democ#session5/Program.cs
@@ -5,40 +5,34 @@ namespace democ_session5
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Dosomeprotectivecode()
         {
-            //Exception handling and protecive code
-            //try && catch    من خلال execepption تهندل ال 
-            //Exception 
-            //1.system exeception 
-            //1.1 format exception
-            //1.2 index out of range exception 
-            //1.3 null reference exception 
-            //1.4 arthimetic exception 
-            //1.4.1 over flow exeception 
-            //1.4.2 divid by zero exeception 
-
-            //2. application exception 
-            try
+            int x, y, z;
+            bool flag;
+            do
             {
-                int x, y, z;
-                x = int.Parse(Console.ReadLine());
-                y = int.Parse(Console.ReadLine());
-
-                z = x / y;
-
-                int[] arr = { 1, 2, 3 };
-
-                arr[99] = 10;
+                Console.WriteLine("enter first number:");
+                flag = int.TryParse(Console.ReadLine(), out x);
             }
-            catch (Exception e)
+            while (flag==false);
+            do
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine("enter second number:");
+                flag = int.TryParse(Console.ReadLine(), out y);
             }
-            finally {
-                Console.WriteLine("finally");
-            // disconnect | dispose un managed resource
+            while (flag == false);
+            while (flag == false) ;
+            do
+            {
+                Console.WriteLine("enter third number:");
+                flag = int.TryParse(Console.ReadLine(), out z);
             }
+            while (flag == false);
         }
+         static void Main()
+        {
+            Dosomeprotectivecode();
+        }
+
     }
-}
+ }

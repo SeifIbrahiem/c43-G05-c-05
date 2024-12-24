3046abaa987032b12a88f1200627ea4b762c7897
Greg Low <greg@gmail.com>
2024-12-24 05:23:17 +0200

    Boxingandunboxing

--- democ#session5/Program.cs
+++ democ#session5/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Boxingandunboxing
+            //boxing>casting from value type to reference type 
+            //un Boxing>casting from reference type to value type
+            // Object O1;
+            // O1 =>can refer to instance of "object" or any instance of any type
+            // O1 = 1;
+            // O1 = "Ahmed";
+            //O1 = 3 ; //case from value type [int] to reference type [object] => Boxing
+            //O1 = 1.5 ; //case from value type [double] to reference type [object] => Boxing
+            //O1 = 'A' ; //case from value type [char] to reference type [object] => Boxing
+            // O1 = new datatime ();// case from value type [date time] to reference type [object] => Boxing
+            //01 = true  ; // case from value type [bool] to reference type [object] => Boxing
+            int X = 4;
+            object O1 = X;
+            //IMPLICIT CASTING 
+            //BOXING 
+            //SAFE CASTING
+            //PARENT = CHILD => CHILD IS PARENT
+            //ANIMAL = DOG => DOG IS ANIMAL
+
+            Object O2 = 3;
+            int Y = (int)O1;
+
+            //EXPLICIT CASTING 
+            //UN BOXING 
+            //UN SAFE CASTING [MAY THROW EXEPTION]
+            //CHILD = PARENT => PARENT IS CHILD
+            //DOG = ANIMAL => ANIMAL IS DOG
+            #endregion
+
+
+
+             
         }
     }
 }
